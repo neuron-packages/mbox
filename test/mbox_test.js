0fd9414dd1b2cf874b1817ddfff9f648fa2288f3
supersheep <supersheepxjy@gmail.com>
2014-06-06 15:47:27 +0800

    update mbox

--- test/mbox_test.js
+++ test/mbox_test.js
@@ -1,42 +1,53 @@
-var mbox = require("mbox");
+var mbox = require("../lib/index");
 var $ = require("jquery");
+var assert = require("assert");
+describe("single mbox", function() {
 
-describe("single mbox", function(){
-    it("should return instance of Mbox", function(){
-        expect(mbox({})).to.be.a("object");
-    });
-    it("should open and close properly", function(){
-        var mybox = mbox({
-            content:"<div>see me</div>"
-        });
+  // it("should fail in ie",function(){
+  //     console.log(navigator.userAgent);
+  //     assert.equal(/msie/i.test(navigator.userAgent),false);
+  // });
 
-        expect(mybox).to.be.a("object");
-        mybox.open();
-        expect($("#mbox_overlay").length).to.equal(1);
-        mybox.close();
-        expect($("#mbox_overlay").length).to.equal(0);
+  it("should return instance of Mbox", function() {
+    assert.equal(typeof mbox({}), "object");
+  });
+
+  it("should open and close properly", function() {
+    var mybox = mbox({
+      content: "<div>see me</div>"
     });
+
+    console.log(mybox);
+    assert.equal(typeof mybox, "object");
+    mybox.open();
+    assert.equal($("#mbox_overlay").length, 1);
+    mybox.close();
+    assert.equal($("#mbox_overlay").length, 0);
+  });
 });
 
-describe("multi mboxes",function(){
+describe("multi mboxes", function() {
 
-    it("should be able to open multi mboxes", function(){
-        var mybox1 = mbox({
-            content:"<div>see me</div>"
-        });
-        var mybox2 = mbox({
-            content:"<div>see me</div>"
-        });
-        var mybox3 = mbox({
-            content:"<div>see me</div>"
-        });
-
-        mybox1.open();mybox2.open();mybox3.open();
-        expect($("#mbox_overlay").length).to.equal(1);
-        mybox1.close();mybox2.close();
-        expect($("#mbox_overlay").length).to.equal(1);
-        mybox3.close();
-        expect($("#mbox_overlay").length).to.equal(0);
+  it("should be able to open multi mboxes", function() {
+    var mybox1 = mbox({
+      content: "<div>see me</div>"
     });
+    var mybox2 = mbox({
+      content: "<div>see me</div>"
+    });
+    var mybox3 = mbox({
+      content: "<div>see me</div>"
+    });
+
+    mybox1.open();
+    mybox2.open();
+    mybox3.open();
+    assert.equal($("#mbox_overlay").length, 1);
+    mybox1.close();
+    mybox2.close();
+    assert.equal($("#mbox_overlay").length, 1);
+    mybox3.close();
+    assert.equal($("#mbox_overlay").length, 0);
+  });
 
 });
